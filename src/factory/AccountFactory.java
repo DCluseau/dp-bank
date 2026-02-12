89ef726ab4a8dfadc87bc246b450f276da617d07
DCluseau <daphne.cluseau@fms-ea.com>
2026-02-12 15:21:45 +0100

    Init factory file

---
+++ src/factory/AccountFactory.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package factory;
+import model.AccountModel;
+
+/**
+ * 
+ */
+public class AccountFactory {
+
+	/**
+	 * 
+	 */
+	public AccountFactory() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void CreateAccount(AccountModel account) {
+		
+	}
+
+}
