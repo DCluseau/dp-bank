fff296b1ac9130e26ee2b0042650e82f6fbe83e7
DCluseau <daphne.cluseau@fms-ea.com>
2026-02-12 15:19:00 +0100

    Added services architecture

---
+++ src/service/AccountService.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package service;
+import dao.AccountDAO;
+import dao.OperationDAO;
+import exception.InsufficientBalanceException;
+
+/**
+ * 
+ */
+public class AccountService {
+
+	/**
+	 * 
+	 */
+	public AccountService() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/service/CustomerService.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package service;
+import dao.CustomerDAO;
+import exception.AccountNotFound;
+/**
+ * 
+ */
+public class CustomerService {
+
+	/**
+	 * 
+	 */
+	public CustomerService() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
